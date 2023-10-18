15accd48fe7ba2cff225f964d37aa4f34948e999
invissy <72313458+invissy@users.noreply.github.com>
2023-10-18 03:27:19 -0400

    Added login.js + components folder

--- art-social-app/App.js
+++ art-social-app/App.js
@@ -1,42 +1,34 @@
 import * as React from 'react';
-import {Button, Text, View} from 'react-native';
+import { Button, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginPage from "./components/Login.js";
 
-function HomePage ({navigation}){
-  return(
+function HomePage({ navigation }) {
+  return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Home</Text>
-    <Button title="Go to Your Profile" 
-    onPress={()=>navigation.navigate('Profile')}/>
-  </View>   
+      <Text>Home</Text>
+      <Button title="Go to Your Profile"
+        onPress={() => navigation.navigate('Profile')} />
+    </View>
   )
 }
-function LoginPage ({navigation}){
-  return(
+function ProfilePage() {
+  return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Login</Text>
-    <Button title="Login" 
-    onPress={()=>navigation.navigate('Home')}/>
-  </View>   
-  )
-}
-function ProfilePage (){
-  return(
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Profile Page</Text>
-  </View>   
+      <Text>Profile Page</Text>
+    </View>
   )
 }
 const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName='Login'>
-    <Stack.Screen name="Login" component={LoginPage} />
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name="Login" component={LoginPage} />
         <Stack.Screen name="Home" component={HomePage} />
         <Stack.Screen name="Profile" component={ProfilePage} />
       </Stack.Navigator>
-</NavigationContainer>);
+    </NavigationContainer>);
 };
 export default App;
---
+++ art-social-app/components/Login.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button, Text, View } from 'react-native';
+import { TextInput } from "react-native-gesture-handler";
+
+export default function LoginPage({ navigation }) {
+
+
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <h1>Login</h1>
+            <TextInput placeholder="Username" />
+            <TextInput placeholder="Password" />
+            <Button
+                title="Log in"
+                onPress={() => navigation.navigate('Home')}
+            />
+
+            <Text>Forgot password?</Text>
+        </View>
+    );
+}
