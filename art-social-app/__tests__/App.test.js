9ce023f21f930b472b5067215863a98fe9870296
Ashna Razdan <ashnarazdan123@gmail.com>
2023-10-26 23:30:21 -0400

    try to change the directory

--- art-social-app/__tests__/App.test.js
+++ art-social-app/__tests__/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
-import App from './App';
+import App from '../App';
 
 test('renders App component without crashing', () => {
     render(<App />);
